e12174f0fd662d9720f476d8d1922cc48a21cb57
Vojnuk <martel.gilgames@gmail.com>
2019-06-22 23:37:18 +0200

    no longer necessary

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-window.addEventListener('load', ()=>{
-
-const nav = document.getElementById('navigation');
-nav.addEventListener('mouseenter', ()=>{
-    let elems = document.body.getElementsByTagName('*');
+window.addEventListener('load', function(){
+/*
+var nav = document.getElementById('navigation');
+nav.addEventListener('mouseenter', function(){
+    var elems = document.body.getElementsByTagName('*');
     elems = Array.from(elems);
-    elems.forEach( elem => {
+    elems.forEach( function(elem) {
         elem.classList.add('testBorder');
     });
     
 })
 
-nav.addEventListener('mouseleave', ()=>{
-    let elems = document.body.getElementsByTagName('*');
+nav.addEventListener('mouseleave', function(){
+    var elems = document.body.getElementsByTagName('*');
     elems = Array.from(elems);
-    elems.forEach( elem => {
+    elems.forEach( function(elem) {
         elem.classList.remove('testBorder');
     });
     
 })
-
+*/
+this.document.createElement('main');
+this.document.createElement('article');
 
 
 
